66b4cf020459e774f9225ffa7cba56149e068724
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-01-24 17:34:43 -0700

    get the data from api

--- src/Row.js
+++ src/Row.js
@@ -1,17 +1,16 @@
 import { React, useState, useEffect } from 'react'; 
-import axios from "../../axios";
+import axios from "./axios";
+import App from "./App";
+import "./Row.css"; 
 
-
-export default function Row({ title , fetchUrl }){
+ function Row({ title , fetchUrl }){
     const [movie, setMovie] = useState([]); //giving the intial value is an empty array
     useEffect(()=>{
         async function fetchData(){
             const request = await axios.get(fetchUrl); 
             console.table(request.data.result); 
-            setMovie(request.data.results);
-            
         }
-
+        fetchData(); 
     }, []) // the reason using the blank bracklet is the code will run once when the page is loaded, and don't run again. 
     
     return(
@@ -27,3 +26,4 @@ export default function Row({ title , fetchUrl }){
         </div>
     ); 
 }
+export default Row;
