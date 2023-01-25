b66f960559cbad2864b2e2047ea11693dc8e5b9d
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-01-25 14:44:17 -0700

    adding effect black for navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,23 @@
 
 //reference: https://commons.wikimedia.org/wiki/File:Netflix-avatar.png
-import React from 'react'; 
+import React , { useState, useEffect } from 'react'; 
 import "./Nav.css"; 
 
 function Nav() {
+    const [show, handleShow] = useState(false); 
+    //create a function which repsone the scorlling 
+    useEffect(() =>{
+        window.addEventListener("scroll", () => {
+            if(window.scrollY > 100) {
+                handleShow(true); 
+            } else handleShow(false); 
+        }); 
+        return () =>{
+            window.removeEventListener("scroll"); 
+        };
+    }, []);
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav_black"}`}>
         <img 
         className="nav_logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
