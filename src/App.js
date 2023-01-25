2d5640d596051a6835731579d721deed3250a6d2
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-01-24 19:56:40 -0700

    insert fetch to app

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ export default function App() {
     return(
         <div>
             {/* each row is one components */}
-            <Row  title="Up Coming" fetchUrl={requests.fetchUpComing}/>
-            <Row  title="Trending Now" fetchUrl={requests.fetchTrending}/>
+            <Row title="Now Playing" fetchUrl={requests.fetchNowPlaying} />
+            <Row title="Latest" fetchUrl={requests.fetchLatest}/>
+            <Row title="Popular" fetchUrl={requests.fetchPopular}/>
+            <Row title="Up Coming" fetchUrl={requests.fetchUpComing}/>
+            <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
         </div>
     );
 }
