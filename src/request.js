9d045b2410c057dfee0d2e99b08fac5b89a7e35d
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-01-24 19:53:12 -0700

    insert three more fectUrl

--- src/request.js
+++ src/request.js
@@ -3,8 +3,11 @@ const API_KEY = "1651d7a28b7fb3b2e688cc7c829dca1b";
 
 
 const requests = {
-    fetchTrending: `/trending/movie/week?api_key=${API_KEY}`,
-    fetchUpComing: `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
+    fetchTrending:  `/trending/movie/week?api_key=${API_KEY}`,
+    fetchUpComing:  `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`, 
+    fetchPopular:   `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`,
+    fetchLatest:    `/movie/latest?api_key=${API_KEY}&language=en-US&page=1`,
+    fetchNowPlaying:`/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`,
   }
 
   export default requests; 
