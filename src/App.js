d20e82b5712e35e9d73638ceb90553c83ae315df
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-01-25 13:06:35 -0700

    insert useEffect in banner

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./App.css"; 
 import Row from "./Row"; 
 import requests from "./request";
-
+import Banner from './Banner'
 
 export default function App() {
     return(
         <div>
+            {/* NavBar */}
+            {/* banner */}
+            <Banner />
             {/* each row is one components */}
             {/* top rated will have the largest piture of movie  */}
             <Row title="Top Rated"      fetchUrl={requests.fetchPopular}    isLargeRow={true}/>
