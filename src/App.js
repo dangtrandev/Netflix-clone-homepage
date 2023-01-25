f39df0412409e0840f83f9e8472d75c289f81c6c
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-01-24 20:08:41 -0700

    deleted fetchNowPlaying

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ export default function App() {
     return(
         <div>
             {/* each row is one components */}
-            {/* <Row title="Now Playing"    fetchUrl={requests.fetchNowPlaying} /> */}
             <Row title="Popular"        fetchUrl={requests.fetchPopular}/>
             <Row title="Up Coming"      fetchUrl={requests.fetchUpComing}/>
             <Row title="Trending Now"   fetchUrl={requests.fetchTrending}/>
