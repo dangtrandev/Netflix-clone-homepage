c84651791fc16cde7090787411aa1d0a4322b009
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-01-24 20:11:30 -0700

    insert fetchTopRated

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default function App() {
     return(
         <div>
             {/* each row is one components */}
+            <Row title="Top Rated"      fetchUrl={requests.fetchPopular}/>
             <Row title="Popular"        fetchUrl={requests.fetchPopular}/>
             <Row title="Up Coming"      fetchUrl={requests.fetchUpComing}/>
             <Row title="Trending Now"   fetchUrl={requests.fetchTrending}/>
