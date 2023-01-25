1a6f8c302c662a8d8ad027bc8ea55d9e883fc391
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-01-25 16:18:02 -0700

    adding firebase to deploy

--- src/App.js
+++ src/App.js
@@ -4,8 +4,23 @@ import Row from "./Row";
 import requests from "./request";
 import Banner from './Banner'; 
 import Nav from './Nav'; 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 export default function App() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyAtKZTwcvaYVKsexMXkmUdFMkm793NVueU",
+        authDomain: "netflix-clone-dangtran.firebaseapp.com",
+        projectId: "netflix-clone-dangtran",
+        storageBucket: "netflix-clone-dangtran.appspot.com",
+        messagingSenderId: "1052618129752",
+        appId: "1:1052618129752:web:6f8af522b960f338bde52d",
+        measurementId: "G-3M64TSTEV6"
+      };
+      
+      // Initialize Firebase
+      const app = initializeApp(firebaseConfig);
+      const analytics = getAnalytics(app);
     return(
         <div className="app">
             {/* NavBar */}
