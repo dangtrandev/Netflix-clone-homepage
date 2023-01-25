c693f3b579f3b393eceb023cdd0b712ed3b4eb01
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-01-24 18:04:36 -0700

    fixing the request with api key

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios'; 
 
 //using base URL to make the request of movie from database 
-/*create the instance so that every single time we do instance.get('/smt') will insert "smt" to the
-tails of the baseURL*/
+//create the instance so that every single time we do instance.get('smt') will insert smt to the tails of baseURl
 const instance = axios.create({baseURL: "https://api.themoviedb.org/3"}); 
 
 export default instance; 
